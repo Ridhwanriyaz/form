b4e030a9a2e0205c8c4abb3334f69ba0f009becf
Ridhwanriyaz <ridhufeb29@gmail.com>
2026-08-10 11:40:59 +0530

    added script js

---
+++ script.js
@@ -0,0 +1,24 @@
+let age = "29";
+
+if (age.length !== 2) {
+    alert("Age should contain exactly 2 digits.");
+}
+
+console.log(age);
+
+function confirmReset() {
+    return confirm("Do you really want to reset the form?");
+}
+
+function checkEmail() {
+    let email = document.getElementById("email").value;
+
+    if (!email.includes(".com")) {
+        alert("Email must contain .com");
+        return false;
+    }
+
+    return true;
+}
+
+
